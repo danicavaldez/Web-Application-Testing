c9abd27373c75227b878cda5e980b49f579e4d01
Danica Valdez <danicavaldez@Danicas-MacBook-Pro.local>
2019-09-15 15:31:02 -0700

    All buttons and functionality work

--- testing-app/src/components/Dashboard.js
+++ testing-app/src/components/Dashboard.js
@@ -10,12 +10,35 @@ export default function Dashboard(props) {
       }
   }
 
+  function addBall() {
+    props.setBall(props.ball + 1)
+    if(props.ball === 4) {
+      props.setStrike(0)
+      props.setBall(0)
+    }
+  }
+
+  function addFoul() {
+    props.setFoul(props.foul + 1)
+    if (props.strike <= 1) {
+      props.setStrike(props.strike + 1)
+    }
+  }
+
+  function addHit() {
+    props.setHit(props.hit + 1)
+    if(props.hit === 1) {
+      props.setStrike(0)
+      props.setBall(0)
+    }
+  }
+
   return (
     <div>
       <button onClick={addStrike}>Strike</button>
-      <button>Ball</button>
-      <button>Foul</button>
-      <button>Hit</button>
+      <button onClick={addBall}>Ball</button>
+      <button onClick={addFoul}>Foul</button>
+      <button onClick={addHit}>Hit</button>
     </div>
   )
 }
