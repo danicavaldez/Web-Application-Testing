91eb1566672c324d91539f0812daf94d554193d5
Danica Valdez <danicavaldez@Danicas-MacBook-Pro.local>
2019-09-15 14:25:10 -0700

    Added base data to display

--- testing-app/src/App.js
+++ testing-app/src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import Display from './components/Display.js'
 
 import './App.css';
 
 function App() {
   return (
     <div>
+      <h1>Baseball App</h1>
+      <Display />
     </div>
   );
 }
---
+++ testing-app/src/components/Display.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Display(){
+  return(
+    <div>
+      <p>Balls:</p>
+      <p>strikes:</p>
+    </div>
+  )
+}
