36032ecbcd1cb5d17f844477bf22c52c04299a0a
Danica Valdez <danicavaldez@Danicas-MacBook-Pro.local>
2019-09-15 15:10:11 -0700

    set state and props in App

--- testing-app/src/App.js
+++ testing-app/src/App.js
@@ -1,15 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Display from './components/Display.js'
 import Dashboard from "./components/Dashboard"
 
 import './App.css';
 
 function App() {
+  const [ball, setBall] = useState(0);
+  const [strike, setStrike] = useState(0);
+  const [hit, setHit] = useState(0);
+  const [foul, setFoul] = useState(0);
+
   return (
     <div>
       <h1>Baseball App</h1>
-      <Display />
-      <Dashboard />
+      <Display ball={ball} setBall={setBall} strike={strike} setStrike={setStrike} foul={foul} setFoul={setFoul} hit={hit} setHit={setHit} />
+      <Dashboard ball={ball} strike={strike} />
     </div>
   );
 }
