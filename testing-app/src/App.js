ff742074e4da42fd0efa4a270a0df0c234a26058
Danica Valdez <danicavaldez@Danicas-MacBook-Pro.local>
2019-09-15 14:29:07 -0700

    Imported dashboard into App and added basic data

--- testing-app/src/App.js
+++ testing-app/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Display from './components/Display.js'
+import Dashboard from "./components/Dashboard"
 
 import './App.css';
 
@@ -8,6 +9,7 @@ function App() {
     <div>
       <h1>Baseball App</h1>
       <Display />
+      <Dashboard />
     </div>
   );
 }
---
+++ testing-app/src/components/Dashboard.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Dashboard() {
+  return (
+    <div>
+      <button>Strike</button>
+      <button>Ball</button>
+      <button>Foul</button>
+      <button>Hit</button>
+    </div>
+  )
+}
